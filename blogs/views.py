01273a06fc0b4deaa0086026f84d2cbddf73c3c2
diodores <s9634898702@gmail.com>
2022-07-05 16:17:13 +0300

    Исправлена ошибка с отобоажением фото

--- blogs/views.py
+++ blogs/views.py
@@ -17,10 +17,10 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     cats = Category.objects.all()
-    posts = Article.objects.filter(pk=cat_id)
+    posts = Article.objects.filter(cat_id=cat_id)
     context = {
         'cats': cats,
         'posts': posts,
         'cat_selected': cat_id
     }
-    return render(request, 'women/index.html', context=context)
+    return render(request, 'blogs/index.html', context=context)
